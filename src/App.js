a2cd84a6e5252fb1733b06abbc23056ef3abab3e
JintoYamanaka <yamajin.ips11@gmail.com>
2023-11-07 11:43:18 +0900

    レイアウト変更

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import { useState } from "react";
 import { css } from "@emotion/react";
 
 const appStyle = css`
-  margin: 0;
-  padding: 0;
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100vh;
-  flex-direction: column;
+  background-color: #f7f7f7;
 `;
 
 const modalStyle = css`
@@ -35,7 +33,20 @@ const listStyle = css`
 `;
 
 const buttonStyle = css`
-  margin-left: 10px;
+  padding: 10px 15px;
+  margin: 0 5px;
+  border: none;
+  border-radius: 20px;
+  background-color: #4caf50;
+  color: white;
+  cursor: pointer;
+  box-shadow: 0 2px 2px 0 rgba(0,0,0,0.15);
+  transition: background-color 0.3s, box-shadow 0.3s;
+
+  &:hover {
+    background-color: #45a049;
+    box-shadow: 0 4px 4px 0 rgba(0,0,0,0.2);
+  }
 `;
 
 const cancelButtonStyle = css`
@@ -46,6 +57,36 @@ const cancelButtonStyle = css`
   &:hover {
     background-color: #d32f2f;
   }
+`;
+
+const listContainerStyle = css`
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  width: 100%;
+  max-width: 400px;
+  background-color: white;
+  padding: 20px;
+  margin: 20px 0;
+`;
+
+const listItemStyle = css`
+  padding: 10px;
+  border-bottom: 1px solid #eee;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  &:last-child {
+    border-bottom: none;
+  }
+`;
+
+const listTitleStyle = css`
+  font-size: 20px;
+  font-weight: bold;
+  margin-bottom: 10px;
+  text-align: center;
 `;
 
 function App() {
@@ -98,15 +139,18 @@ function App() {
           </div>
         </div>
       )}
-      <div css={listStyle}>
-        {tasks.map((task, index) => (
-          <div key={index}>
-            {task}
-            <button onClick={() => deleteTask(index)} css={buttonStyle}>
-              削除
-            </button>
-          </div>
-        ))}
+      <div css={listContainerStyle}>
+        <div css={listTitleStyle}>TODOリスト</div>
+        {tasks.length > 0 ? (
+          tasks.map((task, index) => (
+            <div key={index} css={listItemStyle}>
+              {task}
+              <button onClick={() => deleteTask(index)} css={buttonStyle}>削除</button>
+            </div>
+          ))
+        ) : (
+          <div css={listItemStyle}>タスクがありません。</div>
+        )}
       </div>
     </div>
   );
